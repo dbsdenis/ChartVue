59a435423996fb4d6ffdbf921c36e386de057ce1
Denilson Silva <dbsdenis@hotmail.com>
2022-10-27 19:25:12 -0300

    Initial commit

---
+++ src/components/Chart.component.vue
@@ -0,0 +1,91 @@
+<template>
+  <apexcharts
+    width="550"
+    type="line"
+    :options="chartOptions"
+    :series="series"
+  ></apexcharts>
+</template>
+
+<script>
+import VueApexCharts from "vue-apexcharts";
+
+let { series, chartOptions } = {
+  series: [
+    {
+      name: "Website Blog",
+      type: "column",
+      data: [440, 505, 414, 671, 227, 413, 201, 352, 752, 320, 257, 160],
+    },
+    {
+      name: "Social Media",
+      type: "line",
+      data: [23, 42, 35, 27, 43, 22, 17, 31, 22, 22, 12, 16],
+    },
+  ],
+  chartOptions: {
+    chart: {
+      height: 350,
+      type: "line",
+      events: {
+        dataPointSelection: function (event, chartContext, config) {
+          console.log(event, chartContext, config);
+        },
+      },
+    },
+    stroke: {
+      width: [0, 4],
+    },
+    title: {
+      text: "Traffic Sources",
+    },
+    dataLabels: {
+      enabled: true,
+      enabledOnSeries: [1],
+    },
+    labels: [
+      "01 Jan 2001",
+      "02 Jan 2001",
+      "03 Jan 2001",
+      "04 Jan 2001",
+      "05 Jan 2001",
+      "06 Jan 2001",
+      "07 Jan 2001",
+      "08 Jan 2001",
+      "09 Jan 2001",
+      "10 Jan 2001",
+      "11 Jan 2001",
+      "12 Jan 2001",
+    ],
+    xaxis: {
+      type: "datetime",
+    },
+    yaxis: [
+      {
+        title: {
+          text: "Website Blog",
+        },
+      },
+      {
+        opposite: true,
+        title: {
+          text: "Social Media",
+        },
+      },
+    ],
+  },
+};
+
+export default {
+  name: "Chart",
+  components: {
+    apexcharts: VueApexCharts,
+  },
+  data: function () {
+    return {
+      chartOptions,
+      series,
+    };
+  },
+};
+</script>
